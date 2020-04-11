4ad6a28388b6201d33707e681e170e7ce0c53177
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-04-11 18:52:10 +0930

    Added avatars

--- public/script.js
+++ public/script.js
@@ -1,30 +1,39 @@
-
-//https://api.github.com/repos/hexagonatron/BootcampW2Activity/commits?author=hexagonatron&since=2020-01-01T00:00:00Z
+//Select page elements
 const tableOutput = document.querySelector(".result-output");
 
+//Fn to process api response
 const processResults = (responseArray) => {
+
+    //Sort response array based on number of commits
     responseArray.sort((a, b) => {
         return b.data.total_count - a.data.total_count;
     });
     console.log(responseArray);
+    
+    //For each user call the create row fn
     responseArray.forEach((person, index) => {
         createRow(person, index + 1);
     });
 }
 
+//Fn to add a row to the page
 const createRow = (person, position) => {
+
+    //HTML template with all info
     htmlString = `
     <tr>
         <td scope="row">${position}</td>
-        <td><a href="https://https://github.com/${person.username}" target="_blank" rel="noopener noreferrer">${person.name}</a></td>
+        <td><img class="git-avatar" src="${person.data.items[0]?person.data.items[0].author.avatar_url:""}"><a href="https://https://github.com/${person.username}" target="_blank" rel="noopener noreferrer">${person.name}</a></td>
         <td>${person.data.total_count}</td>
         <td>${person.data.total_count? `<a href="${person.data.items[0].repository.html_url}" target="_blank" rel="noopener noreferrer">${person.data.items[0].repository.name}</a>`:"No commits =("}</td>
         <td>${person.data.total_count? moment(person.data.items[0].commit.author.date).fromNow():"-"}</td>
     </tr>`;
 
+    //Push to page
     tableOutput.innerHTML += htmlString;
 }
 
+//Query api to get everyones commits
 $.ajax({
     url: "/api",
     method: "GET",
@@ -33,49 +42,3 @@ $.ajax({
         console.log(`Status: ${status}, Error: ${err}`);
     }
 });
-
-
-/* people.forEach(person => {
-
-    var gitUser = person[1];
-
-    var url = `https://api.github.com/users/${gitUser}/repos`;
-
-    console.log(url);
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        var commitCount = 0;
-        console.log(data);
-        var repoCount = data.length;
-        console.log(repoCount);
-
-        data.forEach(repo => {
-
-            var repoName = repo.name;
-
-            var repoUrl = `https://api.github.com/repos/${gitUser}/${repoName}/commits?since=${yearAgoISO}&author=${gitUser}`
-
-            console.log(repoUrl);
-
-            fetch(repoUrl)
-            .then(repoResponse => {
-                console.log(repoResponse)
-                repoResponse.json()
-            })
-            .then(function(repoData){
-                console.log(repoData);
-                commitCount += repoData.length;
-                console.log(commitCount);
-
-            })
-        })
-
-        console.log(commitCount);
-        return commitCount;
-    })
-    .then(function(count){
-        console.log(count);
-    })
-}); */
