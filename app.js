88c55084b64633b36bef0d451fd9d42103244be9
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-04-11 14:47:49 +0930

    Changed to commit api endpoint

--- app.js
+++ app.js
@@ -18,36 +18,64 @@ app.use(express.json({ limit: '1mb' }));
 
 
 app.get('/api', (req, res) => {
-    const gitUsers = ["hexagonatron","bhamann-collab","Alex-Waite","gitcommitscoreboard"];
-    const API_KEY = process.env.API_KEY;
 
-    let allCommits = [];
+    const queryGitHub = (url) => {
+        return new Promise(resolve => {
 
-    resultsArray = gitUsers.map(user => {
-        const url = `https://api.github.com/users/${user}/events`
-        return new Promise(async resolve => {
-
-            const response = await fetch(url, {
+            fetch(url, {
                 method: 'GET',
                 headers: {
-                    'Authorization': 'Basic ' + btoa(`hexagonatron:${API_KEY}`)
+                    'Authorization': 'Basic ' + btoa(`hexagonatron:${API_KEY}`),
+                    'Accept': "application/vnd.github.cloak-preview"
                 }
-            });
-            responseHeaders = await response.headers
-            // console.log(responseHeaders.get("link"));
-            responseJSON = await response.json().then(data => {
-                if(response.headers.has("link")){
-                    console.log(response.headers.get("link").split(','));
-
-                }
-                resolve(data);
-            });
-            // console.log(responseJSON);
+            })
+                .then(APIresponse => {
+                    console.log("#################  API Response  #################");
+                    // console.log(APIresponse);
+                    APIresponse.json().then(responseJSON => {
+                        console.log("#################  ResponseJSON  #################");
+                        // console.log(responseJSON);
+                        // userEvents = [...userEvents, responseJSON];
+                        if (APIresponse.headers.has("link")) {
+                            console.log("#################  Header next url  #################");
+                            let nextURL = APIresponse.headers.get("link");
+                            console.log("\nRaw: " + nextURL+"\n");
+                            urlRegex = /https:\/\/api\.github\.com\/search\/commits\?q=author%3A[\w-]+?&sort=author-date&order=desc&page=\d+?(?=>; rel="next")/g;
+                            nextURL = urlRegex.exec(nextURL);
+                            console.log(nextURL);
+                            
+                            if (nextURL){
+                                console.log("\nNext URL: " + nextURL[0]+ "\n");
+                                queryGitHub(nextURL[0]).then(recursionResults => {
+                                    // console.log(recursionResults);
+                                    resolve([...responseJSON.items, ...recursionResults]);
+                                });
+                            } else {
+                                // console.log(responseJSON);
+                                resolve(responseJSON.items);
+                            }
+                        } else {
+                            // console.log(responseJSON);
+                            resolve(responseJSON.items);
+                        }
+                    });
+                });
         });
-        
+
+    }
+
+    const gitUsers = ["hexagonatron", "bhamann-collab", "Alex-Waite", "gitcommitscoreboard"];
+    // const gitUsers = ["hexagonatron"];
+    const API_KEY = process.env.API_KEY;
+
+    resultsArray = gitUsers.map(user => {
+        let userEvents = [];
+        const url = `https://api.github.com/search/commits?q=author:${user}&sort=author-date&order=desc`;
+        return queryGitHub(url);
     });
 
-    let fetchedData = Promise.all(resultsArray).then(data => {
+    Promise.all(resultsArray).then(data => {
+
         res.json(data);
     });
 });
